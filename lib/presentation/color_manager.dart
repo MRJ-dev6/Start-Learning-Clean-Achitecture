2ef44768fcf7916cd7f6e2971ec80274a3863780
MRJ-dev6 <m44734715@gmail.com>
2026-05-08 20:53:59 +0300

    add ColorManager class with color constants

---
+++ lib/presentation/color_manager.dart
@@ -0,0 +1,7 @@
+class ColorManager {
+  static const int primaryColor = 0xFFED9728;
+  static const int darkgray = 0xFF737477;
+  static const int gray = 0xFF797979;
+  static const int lightgray = 0xFF9E9E9E;
+  
+}
