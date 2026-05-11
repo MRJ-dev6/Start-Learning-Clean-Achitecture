8df2498bf867e23352f32cb9228bb17bed995814
MRJ-dev6 <m44734715@gmail.com>
2026-05-11 21:54:37 +0300

    Enhance AppConstants documentation; clarify purpose and benefits of the constants manager class for better maintainability

--- lib/presentation/resources/constants_manager.dart
+++ lib/presentation/resources/constants_manager.dart
@@ -1,3 +1,8 @@
 class AppConstants {
   static const int splashDelay = 3; // ? this is the delay time for the splash screen in seconds
 }
+//! this class is used to manage the constants of the app and to avoid hardcoding the constants in the code and to make it easier to change the constants in the future if needed.
+//? Benefit of using a constants manager class :
+//1. It helps to keep the code organized and maintainable.
+//2. It allows us to change the constants of the app in one place, and it will be reflected in all the places where we use these constants.
+//3. It helps to avoid hardcoding constants in the code, and it makes it easier to change the constants of the app in the future if needed.
